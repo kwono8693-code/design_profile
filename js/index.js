5cf9d779deb99b17f08cee25c80e7026402647df
권오영 <kwono8693@naver.com>
2026-04-13 18:02:03 +0900

    260413 추가

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded",()=>{
 
     // 위로 스크롤
-    const btnTop = document.querySelector(".btn-top")
-    btnTop.addEventListener("click",()=>{
-        window.scrollTo({top:0,behavior:"smooth"})
-    })
+     const btnTop = document.querySelector(".btn-top")
+    if(btnTop){
+        btnTop.addEventListener("click",()=>{
+            window.scrollTo({top:0,behavior:"smooth"})
+        })
+    }
 
     // 스티커 제목 연해지기
     const detailTitle = document.querySelector(".detail-title")
@@ -17,6 +19,30 @@ document.addEventListener("DOMContentLoaded",()=>{
         detailTitle.style.opacity = opacity
     })
 
+    // 전체 메뉴 버튼
+    const btnMenu = document.querySelector(".btn-menu")
+    const smartMenu = document.querySelector(".smart-menu")
+    let statusMenu = false
+    const btnImg = document.querySelector(".btn-img")
 
+    btnMenu.addEventListener("click",()=>{
+        if(statusMenu){
 
+            smartMenu.classList.remove("on")
+            btnImg.setAttribute('src','./img/btn-menu.svg')
+            statusMenu = false
+        }else{
+
+            smartMenu.classList.add("on")
+            btnImg.setAttribute('src','./img/btn-close-white.svg')
+            statusMenu = true
+        }
+    })
+
+    window.addEventListener('scroll',()=>{
+        smartMenu.classList.remove("on")
+        btnImg.setAttribute('src','./img/btn-menu.svg')
+        statusMenu = false
+    })
+    
 })
